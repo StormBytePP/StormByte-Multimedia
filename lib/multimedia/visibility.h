8b648fa647acd4d348017ba29ead360a3eac2d74
David C. Manuelda <StormByte@gmail.com>
2025-03-07 01:14:01 +0100

    Updated visibility

--- lib/multimedia/visibility.h
+++ lib/multimedia/visibility.h
@@ -3,13 +3,13 @@
 #include <definitions.h>
 
 #ifdef WINDOWS
-	#ifdef StormByte_Config_EXPORTS
-		#define STORMBYTE_CONFIG_PUBLIC	__declspec(dllexport)
+	#ifdef StormByte_Multimedia_EXPORTS
+		#define STORMBYTE_MULTIMEDIA_PUBLIC	__declspec(dllexport)
   	#else
-      	#define STORMBYTE_CONFIG_PUBLIC	__declspec(dllimport)
+      	#define STORMBYTE_MULTIMEDIA_PUBLIC	__declspec(dllimport)
   	#endif
-  	#define STORMBYTE_CONFIG_PRIVATE
+  	#define STORMBYTE_MULTIMEDIA_PRIVATE
 #else
-    #define STORMBYTE_CONFIG_PUBLIC		__attribute__ ((visibility ("default")))
-    #define STORMBYTE_CONFIG_PRIVATE	__attribute__ ((visibility ("hidden")))
+    #define STORMBYTE_MULTIMEDIA_PUBLIC		__attribute__ ((visibility ("default")))
+    #define STORMBYTE_MULTIMEDIA_PRIVATE	__attribute__ ((visibility ("hidden")))
 #endif
