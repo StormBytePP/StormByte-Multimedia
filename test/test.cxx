1de7af42c35ddf9f8c3df6e3eacf70a3e8919c45
David C. Manuelda <StormByte@gmail.com>
2025-12-30 05:29:11 +0100

    Test should work even with GPL disabled

--- test/test.cxx
+++ test/test.cxx
@@ -12,8 +12,8 @@ int test_num_streams_in_file() {
 	auto expected_file = StormByte::Multimedia::File::Open(test_hdr_plus_file);
 	if (!expected_file) {
 		std::cerr << "Error opening file: " << expected_file.error()->what() << std::endl;
-		std::cerr << "Not failing as tests are not properly prepared yet for non-GPL FFmpeg builds." << std::endl;
-		return 0;
+		// Even with GPL disabled the decoder should be available
+		return 1;
 	}
 	ASSERT_EQUAL(fn_name, 6, std::distance(expected_file->StreamsBegin(), expected_file->StreamsEnd()));
 	return 0;
