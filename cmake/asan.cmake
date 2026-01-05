74bbdd1fcfc93f5dbf9781c0e1482c316fd501ae
David C. Manuelda <StormByte@gmail.com>
2026-01-05 01:18:03 +0100

    Add Windows library link

--- cmake/asan.cmake
+++ cmake/asan.cmake
@@ -1,4 +1,7 @@
 option(ENABLE_ASAN "Enable AddressSanitizer (and UBSan) for debug builds" OFF)
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+	set(ENABLE_ASAN OFF CACHE BOOL "Disable ASan for Release builds" FORCE)
+endif()
 
 if(ENABLE_ASAN AND NOT STORMBYTE_AS_DEPENDENCY AND NOT WIN32)
 	# Recommend Debug builds for ASan
