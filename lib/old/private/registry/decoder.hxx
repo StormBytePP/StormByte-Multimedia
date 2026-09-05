d3593a304111619ac7b7bb6b57bab41f9d6c9e5b
David C. Manuelda <StormByte@gmail.com>
2026-09-05 12:15:12 +0200

    docs: apply dual-license file headers

--- lib/old/private/registry/decoder.hxx
+++ lib/old/private/registry/decoder.hxx
@@ -3,19 +3,26 @@
  *
  * This file is part of StormByte-Multimedia.
  *
- * StormByte-Multimedia is dual-licensed under the following terms:
+ * StormByte-Multimedia original source is dual-licensed:
  *
  * 1. GNU Lesser General Public License v3.0 (or later)
- *    You can redistribute it and/or modify it under the terms of the
+ *    You may redistribute and/or modify this file under the terms of the
  *    GNU Lesser General Public License as published by the Free Software
  *    Foundation, either version 3 of the License, or (at your option)
  *    any later version.
  *
  * 2. Commercial license
- *    Alternatively, this software may be used under the terms of a
- *    commercial license agreement with the sole copyright holder
+ *    Alternatively, this file may be used under the terms of a commercial
+ *    license agreement with the copyright holder
  *    (David C. Manuelda <StormByte@gmail.com>).
- *    Contact the copyright holder for more information.
+ *
+ * Both licenses apply only to original StormByte-Multimedia source in this
+ * file. Third-party components — including FFmpeg and embedded trained data —
+ * remain under their own licenses and are not covered by the commercial grant.
+ *
+ * Neither license grants any patent rights. Any patent licenses required
+ * to use this software or third-party components must be obtained separately
+ * from the patent holders.
  *
  * StormByte-Multimedia is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,7 +30,10 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this StormByte-Multimedia. If not, see <https://www.gnu.org/licenses/>.
+ * version 3 along with StormByte-Multimedia. If not, see
+ * <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-StormByte-Commercial
  */
 
 #pragma once
